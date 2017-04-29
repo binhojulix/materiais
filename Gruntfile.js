204a4867d18c77edbe1f7ba9be9601e57830d54b
binhojulix <fabio.jdluz@gmail.com>
2017-04-29 14:25:27 -0300

    projeto configurado

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 //npm instll grunt --save-dev
 //npm install grunt-cli -g
+//bower
 //npm install grunt-contrib-copy
 
 module.exports = function(grunt){
@@ -21,15 +22,44 @@ module.exports = function(grunt){
 			dis:{
 				src: 'app/dist'
 			}
-		}
+		},
+
+		 less: {
+	      development: {
+	        files: {
+	          "app/public/css/estilo_produtos.css":
+	           "app/public/css/estilo_produtos.less" // Caminho dos arquivos
+	        }
+	      }
+
+    	},
+
+    	watch: {
+		  styles: {
+		    files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
+		    tasks: ['less']
+		 }
+		} 
+
 	
 	});
+
 	//registra ordens das tarefas
-	grunt.registerTask('dist',['clean', 'copy']);
+	grunt.registerTask('dist', ['clean', 'copy']);
+	grunt.registerTask('compilarless',['less', 'watch']);
 	grunt.registerTask('default','dist');
 
 
 	//carrega as dependecias
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');//copia diretorio
+	grunt.loadNpmTasks('grunt-contrib-clean');//apaga diretorio
+	grunt.loadNpmTasks('grunt-contrib-less');//apaga diretorio
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  //mimificar
+  //juntar arquivos
+  //uglify
+  //otimizar imagens
+  //automatizar less
+	//gzip
+	//cache	
 }
